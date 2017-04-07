1d9e0535208d929ba38cce55243cf325b7c29125
adel.amodwala <adel.amodwala@ethoca.com>
2017-04-07 10:55:19 -0400

    add response body annotation to home root

--- src/main/java/com/demo/HomeController.java
+++ src/main/java/com/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.demo;
 import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by adel.amodwala on 4/7/2017.
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public HelloDTO sayHello(RequestEntity request) {
+    public @ResponseBody HelloDTO sayHello(RequestEntity request) {
         return new HelloDTO("what's up", request);
     }
 }
